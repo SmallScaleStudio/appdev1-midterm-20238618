5749445994afa5f102404da4a66be015f13a3eb6
pasion_y <pasion_y@ca01.ub.lan>
2026-03-23 08:24:27 +0800

    implemented task lisk with bootstrap badges and toggle logic

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TaskService } from '../../services/task.services';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './task-list.component.html'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  onDelete(id: number): void {
+    if (confirm('Are you sure you want to delete this task?')) {
+      this.taskService.deleteTask(id);
+      this.tasks = this.taskService.getTasks(); // Refresh list
+    }
+  }
+
+  onToggle(id: number): void {
+    this.taskService.toggleStatus(id);
+  }
 }
