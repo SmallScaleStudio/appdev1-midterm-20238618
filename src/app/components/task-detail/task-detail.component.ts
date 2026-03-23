7e4d651cd634565b51bb9d4822ef92f5a469f349
pasion_y <pasion_y@ca01.ub.lan>
2026-03-23 08:27:19 +0800

    implemented parameterized routig and child route data fetching

--- src/app/components/task-detail/task-detail.component.ts
+++ src/app/components/task-detail/task-detail.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TaskService } from '../../services/task.services';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-detail',
-  imports: [],
-  templateUrl: './task-detail.component.html',
-  styleUrl: './task-detail.component.css'
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './task-detail.component.html'
 })
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit {
+  task: Task | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService
+  ) {}
+
+  ngOnInit(): void {
+    // Read the :id from the URL
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.task = this.taskService.getTaskById(id);
+
+    if (!this.task) {
+      alert('Task not found!');
+    }
+  }
 }
--- src/app/components/task-info/task-info.component.ts
+++ src/app/components/task-info/task-info.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from '../../services/task.services';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-info',
-  imports: [],
-  templateUrl: './task-info.component.html',
-  styleUrl: './task-info.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div *ngIf="task" class="mt-3">
+      <p><strong>Description:</strong> {{ task.description }}</p>
+      <p><strong>Due Date:</strong> {{ task.dueDate | date:'fullDate' }}</p>
+      <p><strong>Priority:</strong> {{ task.priority }}</p>
+      <p><strong>Status:</strong> {{ task.status }}</p>
+    </div>
+  `
 })
-export class TaskInfoComponent {
+export class TaskInfoComponent implements OnInit {
+  task: Task | undefined;
 
+  constructor(private route: ActivatedRoute, private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.parent?.snapshot.paramMap.get('id'));
+    this.task = this.taskService.getTaskById(id);
+  }
 }
