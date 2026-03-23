6ace32eed8ff143ac36690e75b5b885a0aa3004e
pasion_y <pasion_y@ca01.ub.lan>
2026-03-23 08:31:31 +0800

    added edit task function

--- src/app/components/task-edit/task-edit.component.ts
+++ src/app/components/task-edit/task-edit.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TaskService } from '../../services/task.services';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-edit',
-  imports: [],
-  templateUrl: './task-edit.component.html',
-  styleUrl: './task-edit.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './task-edit.component.html'
 })
-export class TaskEditComponent {
+export class TaskEditComponent implements OnInit {
+  task!: Task; 
 
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.parent?.snapshot.paramMap.get('id'));
+    const existingTask = this.taskService.getTaskById(id);
+
+    if (existingTask) {
+
+      this.task = { ...existingTask };
+    } else {
+      alert('Task not found');
+      this.router.navigate(['/tasks']);
+    }
+  }
+
+  onUpdate(): void {
+    this.taskService.updateTask(this.task);
+    alert('Task updated successfully!');
+  
+    this.router.navigate(['/tasks', this.task.id, 'info']);
+  }
 }
